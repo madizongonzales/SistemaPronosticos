fa234179b8359f30de5cebf41f527ab1ebec0f54
Manuel Quevedo Oliva <oscarmanuel7612@gmail.com>
2023-11-22 21:40:09 -0400

    se subio RL

--- calcular copy.php
+++ calcular copy.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Resultado del Pronóstico</title>
 </head>
+
 <body>
     <h1>Resultado del Pronóstico</h1>
-    <table>
-        <tr>
-            <th>Periodo</th>
-            <th>Demanda</th>
-            <th>Promedio Móvil Simple</th>
-        </tr>
+
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $demandas = $_POST["demanda"];
@@ -25,10 +22,10 @@
             for ($i = 0; $i < count($demandas); $i++) {
                 $periodo = $i + 1;
                 $demanda = $demandas[$i];
-                if ($n != null){
+                if ($n != null) {
                     $pronostico = ($i >= $n) ? calcularPromedioMovilSimple($demandas, $i, $n) : "";
                     echo "<tr><td>$periodo</td><td>$demanda</td><td>$pronostico</td></tr>";
-                } else if ($t1 != null){
+                } else if ($t1 != null) {
                     $pronostico = ($i >= $n) ? calcularPromedioMovilPonderado($demandas, $i, $t1, $t2, $t3) : "";
                     echo "<tr><td>$periodo</td><td>$demanda</td><td>$pronostico</td></tr>";
                 }
@@ -38,34 +35,69 @@
         }
     }
 
-    function calcularPromedioMovilSimple($demandas, $indice, $n) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Obtener los valores del formulario
+            $a = isset($_POST['a_regresionlineal']) ? floatval($_POST['a_regresionlineal']) : 0;
+            $b = isset($_POST['b_regresionlineal']) ? floatval($_POST['b_regresionlineal']) : 0;
+            $periodos = isset($_POST['periodos']) ? intval($_POST['periodos']) : 0;
+
+            // Mostrar el número de períodos
+            echo "Períodos: $periodos<br>";
+
+            // Calcular el resultado de la regresión lineal para cada período
+            // for ($i = 1; $i <= $periodos; $i++) {
+            //     $resultado = $a + ($b * $i);
+            //     echo "Resultado para el período $i: $resultado<br>";
+            // }
+        }
+    }
+
+    function calcularPromedioMovilSimple($demandas, $indice, $n)
+    {
         // Asegura que el índice no sea negativo
         $indice = max($indice, 0);
-    
+
         // Calcula el promedio de las últimas "n+1" demandas sin contar la última
         $suma = 0;
         for ($i = $indice - $n; $i < $indice; $i++) {
             $suma += $demandas[$i];
         }
-    
+
         return $suma / $n; // Calcula el promedio de las últimas "n+1" demandas
     }
-    function calcularPromedioMovilPonderado($demandas, $indice, $t1, $t2, $t3) {
+    function calcularPromedioMovilPonderado($demandas, $indice, $t1, $t2, $t3)
+    {
         // Asegura que el índice no sea negativo
         $indice = max($indice, 0);
-    
+
         if ($indice < 3) {
             return ""; // No se pueden calcular pronósticos para los primeros 3 periodos
         }
-    
+
         // Calcula el promedio ponderado de los últimos 3 periodos
         $pronostico = ($demandas[$indice - 1] * $t1 + $demandas[$indice - 2] * $t2 + $demandas[$indice - 3] * $t3);
-    
+
         return $pronostico;
     }
-    
+
     ?>
-    
+    <table>
+        <tr>
+            <th>Periodo</th>
+
+            <th>Demanda</th>
+            <th>Promedio Móvil Simple</th>
+            <th>Regresion Lineal</th>
+            <td> <?php for ($i = 1; $i <= $periodos; $i++) {
+                        $resultado = round($a + ($b * $i));
+                        echo "$resultado<br>";
+                    } ?>
+            </td>
+        </tr>
+
     </table>
 </body>
+
 </html>
--- calcular.php
+++ calcular.php
@@ -1,5 +1,5 @@
 <html>
-    
+
 </html>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -15,11 +15,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Realizar los cálculos según el método seleccionado
     $pronosticos = calcularPronosticos($metodo, $numPeriodos, $demanda);
-
 }
-?>
-<?php
-function calcularPronosticos($metodo, $numPeriodos, $demanda) {
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Obtener los valores del formulario
+        $a = isset($_POST['a_regresionlineal']) ? floatval($_POST['a_regresionlineal']) : 0;
+        $b = isset($_POST['b_regresionlineal']) ? floatval($_POST['b_regresionlineal']) : 0;
+        $periodos = isset($_POST['periodos']) ? intval($_POST['periodos']) : 0;
+
+        // Mostrar el número de períodos
+        echo "Períodos: $periodos<br>";
+
+        // Calcular el resultado de la regresión lineal para cada período
+        // for ($n = 1; $n <= $periodos; $n++) {
+        //     $resultado = $a + ($b * $n);
+        //     echo "Resultado para el período $n: $resultado<br>";
+        // }
+    }
+}
+
+function calcularPronosticos($metodo, $numPeriodos, $demanda)
+{
     $pronosticos = array();
 
     for ($i = 0; $i < $numPeriodos; $i++) {
@@ -29,7 +47,8 @@ function calcularPronosticos($metodo, $numPeriodos, $demanda) {
     return $pronosticos;
 }
 
-function calcularPronostico($metodo, $demanda, $indice) {
+function calcularPronostico($metodo, $demanda, $indice)
+{
     $indice = max($indice, 0);
     $n = intval($_POST["n_promedio_movil_simple"]);
     if ($metodo === "n_promedio_movil_simple") {
@@ -37,40 +56,40 @@ function calcularPronostico($metodo, $demanda, $indice) {
             return ""; // No se pueden calcular pronósticos para los primeros $n periodos
         }
         $indice = max($indice, 0);
-    
+
         // Calcula el promedio de las últimas "n" demandas sin contar la última
         $suma = 0;
         for ($i = $indice - $n; $i < $indice; $i++) {
             $suma += $demanda[$i];
         }
-    
+
         $pronostico = $suma / $n; // Calcula el promedio móvil simple
-    
+
         return $pronostico; // Valor calculado según el método
-// }elseif ($metodo === "promedio-movil-ponderado") {
-//     // Lógica para promedio móvil ponderado
-//     // ...
-//     return $pronostico; // Valor calculado según el método
-// }elseif ($metodo === "alfa_suavizado_exponencial_simple") {
-//     // Lógica para promedio móvil ponderado
-//     // ...
-//     return $pronostico; // Valor calculado según el método
-// }elseif ($metodo === "suavizado_exponencial_doble") {
-//     // Lógica para promedio móvil ponderado
-//     // ...
-//     return $pronostico; // Valor calculado según el método
-// }elseif ($metodo === "winters") {
-//     // Lógica para promedio móvil ponderado
-//     // ...
-//     return $pronostico; // Valor calculado según el método
-// }elseif ($metodo === "regresionlineal") {
-//     // Lógica para promedio móvil ponderado
-//     // ...
-//     return $pronostico; // Valor calculado según el método
+        // }elseif ($metodo === "promedio-movil-ponderado") {
+        //     // Lógica para promedio móvil ponderado
+        //     // ...
+        //     return $pronostico; // Valor calculado según el método
+        // }elseif ($metodo === "alfa_suavizado_exponencial_simple") {
+        //     // Lógica para promedio móvil ponderado
+        //     // ...
+        //     return $pronostico; // Valor calculado según el método
+        // }elseif ($metodo === "suavizado_exponencial_doble") {
+        //     // Lógica para promedio móvil ponderado
+        //     // ...
+        //     return $pronostico; // Valor calculado según el método
+        // }elseif ($metodo === "winters") {
+        //     // Lógica para promedio móvil ponderado
+        //     // ...
+        //     return $pronostico; // Valor calculado según el método
+        // }elseif ($metodo === "regresionlineal") {
+        //     // Lógica para promedio móvil ponderado
+        //     // ...
+        //     return $pronostico; // Valor calculado según el método
 
-// return ""; // Método no reconocido
+        // return ""; // Método no reconocido
 
-}
+    }
 }
 ?>
 <table>
@@ -78,14 +97,19 @@ function calcularPronostico($metodo, $demanda, $indice) {
         <th>Periodo</th>
         <th>Demanda</th>
         <th>Pronóstico</th>
+        <th>Regresion Lineal</th>
     </tr>
     <?php
     for ($i = 0; $i < $numPeriodos; $i++) {
         echo "<tr>";
         echo "<td>" . ($i + 1) . "</td>"; // Mostrar el periodo
         echo "<td>" . $demanda[$i] . "</td>"; // Mostrar la demanda
-        echo "<td>" . $pronosticos[$i] . "</td>"; // Mostrar el pronóstico
+        echo "<td>" . $pronosticos[$i] . "</td>";
+        $resultado = $a + ($b * ($i + 1));
+        echo "<td>" . "$resultado<br>" . "</td>"; // Mostrar el pronóstico
+
         echo "</tr>";
     }
     ?>
+
 </table>
